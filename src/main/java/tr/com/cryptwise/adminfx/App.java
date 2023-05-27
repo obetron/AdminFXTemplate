2236c62b10c4f682e95cd8aa214912b476b5a57c
obetron <erenbasaran@gmail.com>
2023-05-27 21:01:34 +0300

    initial commit

---
+++ src/main/java/tr/com/cryptwise/adminfx/App.java
@@ -0,0 +1,11 @@
+package tr.com.cryptwise.adminfx;
+
+/**
+ * @author obetron
+ * @date 27.05.2023 - 21:00
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World AdminFXTemplate");
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,6 @@
+/**
+ * @author obetron
+ * @date 27.05.2023 - 21:01
+ */
+public class AppTest {
+}
