3456c1946f6d3277e11f47e8775edc65e87505f9
obetron <erenbasaran@gmail.com>
2023-05-30 22:03:40 +0300

    first java fx changes added to the project.

--- src/main/java/tr/com/cryptwise/adminfx/App.java
+++ src/main/java/tr/com/cryptwise/adminfx/App.java
@@ -1,11 +1,37 @@
 package tr.com.cryptwise.adminfx;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * @author obetron
  * @date 27.05.2023 - 21:00
  */
-public class App {
+public class App extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World AdminFXTemplate");
+        launch();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/MainTemplate.fxml"));
+        Scene rootScene = new Scene(root);
+        stage.setResizable(false);
+        stage.setTitle("AdminFX Template");
+        stage.setScene(rootScene);
+        stage.show();
     }
 }
